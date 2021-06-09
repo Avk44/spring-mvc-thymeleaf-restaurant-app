084a9395faa4c56b30fdd5f67ddd8dddacde80a7
abdulvahap karabekmez <abdulvahap.karabekmez@gmail.com>
2021-06-09 15:13:12 +0200

    initial commit

---
+++ src/test/java/be/multimedi/restoapp/RestoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package be.multimedi.restoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
