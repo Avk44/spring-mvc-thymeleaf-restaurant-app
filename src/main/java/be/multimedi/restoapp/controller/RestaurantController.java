e070bb397138e2469e8d68703e7186291d500d74
abdulvahap karabekmez <abdulvahap.karabekmez@gmail.com>
2021-06-10 23:04:44 +0200

    added restaurant form template

--- src/main/java/be/multimedi/restoapp/controller/RestaurantController.java
+++ src/main/java/be/multimedi/restoapp/controller/RestaurantController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -37,5 +38,11 @@ public class RestaurantController {
     public String showRestaurantAddForm(Model model){
         model.addAttribute("restaurant",new Restaurant());
         return "restaurant-form";
+    }
+
+    @PostMapping("/restaurant/new")
+    public String fetchMenuById(Restaurant restaurant){
+        restaurantService.register(restaurant);
+        return "restaurant-detail";
     }
 }
